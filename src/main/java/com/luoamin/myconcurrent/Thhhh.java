e7fc5434f6f3ea500dcb9122a0c39a4b1e1741bc
softluoyonghao <2293121112@qq.com>
2018-07-12 14:12:22 +0800

    测试支持类

---
+++ src/main/java/com/luoamin/myconcurrent/Thhhh.java
@@ -0,0 +1,10 @@
+package com.luoamin.myconcurrent;
+
+/**
+ * Created by Administrator on 2018/7/12.
+ */
+public class Thhhh  implements Runnable{
+    public void run(){
+        System.out.println("plase give me a kiss!");
+    }
+}

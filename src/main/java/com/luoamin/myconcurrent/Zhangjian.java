58e310a668ab0bce44c60a31b1df0572f6c9a854
softluoyonghao <2293121112@qq.com>
2018-07-13 09:56:42 +0800

    张建测试1

--- src/main/java/com/luoamin/myconcurrent/Zhangjian.java
+++ src/main/java/com/luoamin/myconcurrent/Zhangjian.java
@@ -4,4 +4,7 @@ package com.luoamin.myconcurrent;
  * Created by Administrator on 2018/7/13.
  */
 public class Zhangjian {
+    public static void main(String[] args) {
+        System.out.println("i ssd ");
+    }
 }

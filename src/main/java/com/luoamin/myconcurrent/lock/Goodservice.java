b918b1756d00e5126eec4a5ad07f83e12a98c0f5
softluoyonghao <2293121112@qq.com>
2018-07-18 21:52:26 +0800

    显示锁和cas

---
+++ src/main/java/com/luoamin/myconcurrent/lock/Goodservice.java
@@ -0,0 +1,9 @@
+package com.luoamin.myconcurrent.lock;
+
+/**
+ * Created by Administrator on 2018/7/18.
+ */
+public interface Goodservice {
+     int getNum();
+     void setNum(int sellNumber);
+}
---
+++ src/main/java/com/luoamin/myconcurrent/lock/Student.java
@@ -0,0 +1,28 @@
+package com.luoamin.myconcurrent.lock;
+
+/**
+ * Created by Administrator on 2018/7/18.
+ */
+public class Student {
+    private String userName;
+    private String age;
+    public Student(String userName,String age){
+        this.userName=userName;
+        this.age=age;
+    }
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+}

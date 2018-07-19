5230cee1215229711a85f4d710f676794f435590
softluoyonghao <2293121112@qq.com>
2018-07-19 16:06:17 +0800

    解决post请求中文乱码问题

--- src/main/java/com/luoamin/web/User/JspController.java
+++ src/main/java/com/luoamin/web/User/JspController.java
@@ -2,6 +2,12 @@ package com.luoamin.web.User;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 /**
  * Created by Administrator on 2018/7/19.
@@ -13,5 +19,18 @@ public class JspController {
     public String encoding(){
         System.out.println("可以进来了！");
         return "/test/encoding";
+    }
+
+    @RequestMapping(value = "toSubmit")
+    public String toSubmit(){
+        System.out.println("可以进来了！");
+        return "/test/submit";
+    }
+    @RequestMapping(value = "submit", method = RequestMethod.POST )
+    public String submit(){
+        Map map=new HashMap();
+        System.out.println("可以进来了！");
+        map.put("sss","key");
+        return "/test/submit";
     }
 }
---
+++ src/main/webapp/WEB-INF/pages/test/submit.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/7/19
+  Time: 15:29
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<form method="post" action="submit">
+    账户：<input name="" type="text">
+    密码：<input name="" type="text">
+    <input type="submit" value="提交">
+</form>
+</body>
+</html>
